2d0ab01f5c09d7625cc340e7d54b8f9d86174bd3
DEV1-28\GA1B <h.shinya0153@gmail.com>
2020-10-20 12:46:34 +0900

    画像データの追加 画像データ取得 背景フラッグの追加

--- LampGame/LampGame/Obj_Block.cpp
+++ LampGame/LampGame/Obj_Block.cpp
@@ -44,30 +44,59 @@ void CObjBlock::Action()
 		m_scroll -= hero->GetVX();
 	}
 
+	/*//敵出現ライン
+	//主人公の位置+500を敵出現ラインにする
+	float line = hx + (-m_scroll) + 500;
 
+	//敵出現ラインを要素番号化
+	int ex = ((int)line) / 64;
+
+	//敵出現ラインの列を検索
+	for (int i = 0; i < 10; i++)
+	{
+		//列の中から5を探す
+		if (m_map[i][ex] == 5)
+		{
+			//5があれば、敵を出現
+			CObjEnemy* obje = new CObjEnemy(ex * 64.0f, i * 64.0f);
+			Objs::InsertObj(obje, OBJ_ENEMY, 10);
+
+			//敵出現場所の値を0にする
+			m_map[i][ex] = 0;
+		}
+	}*/
 }
 
 //ドロー
 void CObjBlock::Draw()
 {
+	int background_flag = 0;
+
 	//描画カラー情報 R=RED　G=Green　B=Blue　A=alpha(透過情報)
 	float  c[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
 
 	RECT_F src;	//描画元切り取り位置
 	RECT_F dst;	//描画先表示位置
 
-	//切り取り位置の設定
-	src.m_top = 0.0f;
-	src.m_left = 0.0f;
-	src.m_right = 64.0f;
-	src.m_bottom = 64.0f;
+	CObjHero* hero = (CObjHero*)Objs::GetObj(OBJ_HERO);
+	bool L_flag_background = hero->Get_L_flag();
+	if (L_flag_background == true)
+		background_flag = 0;
+	else
+		background_flag = 1;
 
-	//表示位置の設定
-	dst.m_top = 0.0f;
-	dst.m_left = 0.0f;
-	dst.m_right = 64.0f;
-	dst.m_bottom = 64.0f;
-
-	//0番目に登録したグラフィックをsrc・dst・c の情報をもとに描画
-	Draw::Draw(0, &src, &dst, c, 0.0f);
+	if (background_flag == 0)
+	{
+		//背景表示
+		src.m_top = 256.0f;
+		src.m_left = 0.0f;
+		src.m_right = 512.0f;
+		src.m_bottom = 512.0f;
+		dst.m_top = 0.0f;
+		dst.m_left = 0.0f;
+		dst.m_right = 800.0f;
+		dst.m_bottom = 600.0f;
+		Draw::Draw(20, &src, &dst, c, 0.0f);
+	}
+	
 }
--- LampGame/LampGame/SceneStage_1.cpp
+++ LampGame/LampGame/SceneStage_1.cpp
@@ -32,32 +32,36 @@ CSceneStage_1::~CSceneStage_1()
 void CSceneStage_1::InitScene()
 {
 	//マップデータ完成したら追加
-	//unique_ptr<wchar_t>p;
-	//int size;
-	//p = Save::ExternalDataOpen(L"Stages.csv", &size);
-	//int map[10][100];
-	//int count = 1;
-	//for (int i = 0; i < 10; i++)
-	//{
-	//	for (int j = 0; j < 100; j++)
-	//	{
-	//		int w = 0;
-	//		swscanf_s(&p.get()[count], L"%d", &w);
-	//		map[i][j] = w;
-	//		count += 2;
-	//	}
-	//}
+	unique_ptr<wchar_t>p;
+	int size;
+	p = Save::ExternalDataOpen(L"Stages.csv", &size);
+	int map[10][100];
+	int count = 1;
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 100; j++)
+		{
+			int w = 0;
+			swscanf_s(&p.get()[count], L"%d", &w);
+			map[i][j] = w;
+			count += 2;
+		}
+	}
 
 	Draw::LoadImageW(L"lamp_walk.png", 0, TEX_SIZE_512);
 	Draw::LoadImageW(L"lamp_shadow_walk.png", 1, TEX_SIZE_512);
+	Draw::LoadImageW(L"stage_background", 20, TEX_SIZE_512);
+	Draw::LoadImageW(L"stage_ground.png", 21, TEX_SIZE_512);
+	Draw::LoadImageW(L"stage_button.png", 22, TEX_SIZE_512);
+	Draw::LoadImageW(L"stage_plate.png", 23, TEX_SIZE_512);
 
 	CObjHero* obj = new CObjHero();
 	Objs::InsertObj(obj, OBJ_HERO, 10);
 
-	/*CObjBlock* objb = new CObjBlock(map);
+	CObjBlock* objb = new CObjBlock(map);
 	Objs::InsertObj(objb, OBJ_BLOCK, 9);
 
-	CObj_G_Block* objg = new CObj_G_Block(map);
+	/*CObj_G_Block* objg = new CObj_G_Block(map);
 	Objs::InsertObj(objb, OBJ_G_BLOCK, 8);
 
 	CObjSwitch* objs = new CObjSwitch;
