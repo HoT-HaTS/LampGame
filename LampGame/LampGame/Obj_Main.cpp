1a7ac89da0c2049387ad173a165d04b9bd6acc30
志水紫音 <h1202019010@hiratagakuen.onmicrosoft.com>
2020-10-20 11:01:10 +0900

    リトライ

--- LampGame/LampGame/Obj_Main.cpp
+++ LampGame/LampGame/Obj_Main.cpp
@@ -2,6 +2,7 @@
 #include "GameL/DrawTexture.h"
 #include "GameL/WinInputs.h"
 #include "GameL/SceneManager.h"
+#include "GameL/HitBoxManager.h"
 
 #include "GameHead.h"
 #include "Obj_Main.h"
@@ -12,36 +13,22 @@ using namespace GameL;
 //イニシャライズ
 void CObjMain::Init()
 {
-
+	
 }
 
 //アクション
 void CObjMain::Action()
 {
-
+	//リトライ
+	if (Input::GetVKey('R') == true)
+	{
+			Scene::SetScene(new CSceneStage_1());
+	}
+	
 }
 
 //ドロー
 void CObjMain::Draw()
 {
-	//描画カラー情報 R=RED　G=Green　B=Blue　A=alpha(透過情報)
-	float  c[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
 
-	RECT_F src;	//描画元切り取り位置
-	RECT_F dst;	//描画先表示位置
-
-	//切り取り位置の設定
-	src.m_top = 0.0f;
-	src.m_left = 0.0f;
-	src.m_right = 64.0f;
-	src.m_bottom = 64.0f;
-
-	//表示位置の設定
-	dst.m_top = 0.0f;
-	dst.m_left = 0.0f;
-	dst.m_right = 64.0f;
-	dst.m_bottom = 64.0f;
-
-	//0番目に登録したグラフィックをsrc・dst・c の情報をもとに描画
-	Draw::Draw(0, &src, &dst, c, 0.0f);
 }
