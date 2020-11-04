001ce7e53306560f41f9775b377f50685bf1c27d
東田　卓也 <h1202019037@hiratagakuen.onmicrosoft.com>
2020-11-04 16:17:58 +0900

    看板仕様追加

--- LampGame/LampGame/Obj_Tutorial.h
+++ LampGame/LampGame/Obj_Tutorial.h
@@ -15,10 +15,13 @@ public:
 	void Action();		//アクション
 	void Draw();		//ドロー
 
-	
+	//void SetNum(int i) { b_number = i; }
+
+
 private:
 	float m_scroll;     //左右スクロール用
 	float m_px;
 	float m_py;
 	bool T_Sensor;
+	int b_number;		//看板番号
 };
