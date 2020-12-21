3d1da6f262e0781a1a023c667bdc1feb374e5424
奏樹 田邊 <h1202019033@hiratagakuen.onmicrosoft.com>
2020-12-21 12:13:26 +0900

    Obj_Clear のマクロ化

--- LampGame/LampGame/ObjClear.cpp
+++ LampGame/LampGame/ObjClear.cpp
@@ -15,38 +15,38 @@ using namespace GameL;
 //イニシャライズ
 void CObjClear::Init()
 {
-	scene_num = 7;
-	alpha_b = 1.0f;
-	alpha_w = 0.0f;
+	scene_num = BG_NUM;
+	alpha_b = INIT_ALPHA_BLACK;
+	alpha_w = INIT_ALPHA_WHITE;
 	white = false;
 	w_end = false;
 	white_out = false;
-	h_ani_time = 0;
-	h_ani_frame = 0;
+	h_ani_time = INIT_ANI_TIME;
+	h_ani_frame = INIT_ANI_FRAME;
 }
 
 //アクション
 void CObjClear::Action()
 {
-	if (white == false && scene_num >= 2)
+	if (white == false && scene_num > BG_LAST)
 	{
-		alpha_b -= 0.005f;
+		alpha_b -= PLUS__ALPHA_BLACK;
 		if (alpha_b <= 0.0f)
 		{
 			white = true;
 			white_out = true;
 		}
 	}
-	if (scene_num < 2)
+	if (scene_num <= BG_LAST)
 	{
-		h_ani_time += 1;
-		if (h_ani_time >= 10)
+		h_ani_time ++;
+		if (h_ani_time >= MAX_H_ANI_TIME)
 		{
-			h_ani_frame += 1;
-			h_ani_time = 0;
-			if (h_ani_frame >= 4)
+			h_ani_frame ++;
+			h_ani_time = INIT_ANI_TIME;
+			if (h_ani_frame >= MAX_H_ANI_FRAME)
 			{
-				h_ani_frame = 0;
+				h_ani_frame = INIT_ANI_FRAME;
 			}
 		}
 
@@ -77,21 +77,21 @@ void CObjClear::Draw()
 	RECT_F dst;	//描画先表示位置
 
 	//切り取り位置の設定
-	src1.m_top = 600.0f;
-	src1.m_left = 0.0f;
-	src1.m_right = 800.0f;
-	src1.m_bottom = 1200.0f;
+	src1.m_top = SRC_BLACK_TOP;
+	src1.m_left = SRC_BLACK_LEFT;
+	src1.m_right = src1.m_left + BG_X_SIZE;
+	src1.m_bottom = src1.m_top + BG_Y_SIZE;
 	//切り取り位置の設定
-	src2.m_top = 0.0f;
-	src2.m_left = 0.0f;
-	src2.m_right = 800.0f;
-	src2.m_bottom = 600.0f;
+	src2.m_top = SRC_WHITE_TOP;
+	src2.m_left = SRC_WHITE_LEFT;
+	src2.m_right = src2.m_left + BG_X_SIZE;
+	src2.m_bottom = src2.m_top + BG_Y_SIZE;
 
 	//表示位置の設定
 	dst.m_top = 0.0f;
 	dst.m_left = 0.0f;
-	dst.m_right = dst.m_left + 800.0f;
-	dst.m_bottom = dst.m_top + 600.0f;
+	dst.m_right = dst.m_left + BG_X_SIZE;
+	dst.m_bottom = dst.m_top + BG_Y_SIZE;
 
 	//描画(影)
 	//描画カラー情報 R=RED　G=Green　B=Blue　A=alpha(透過情報)
@@ -117,93 +117,93 @@ void CObjClear::Draw()
 		//切り取り位置の設定
 		src_w.m_top = 0.0f;
 		src_w.m_left = 0.0f;
-		src_w.m_right = 800.0f;
-		src_w.m_bottom = 600.0f;
+		src_w.m_right = src_w.m_left + BG_Y_SIZE;
+		src_w.m_bottom = src_w.m_top + BG_X_SIZE;
 
 		//表示位置の設定
 		dst_w.m_top = 0.0f;
 		dst_w.m_left = 0.0f;
-		dst_w.m_right = dst_w.m_left + 800.0f;
-		dst_w.m_bottom = dst_w.m_top + 600.0f;
+		dst_w.m_right = dst_w.m_left + BG_X_SIZE;
+		dst_w.m_bottom = dst_w.m_top + BG_Y_SIZE;
 
 
 		//flagがtrueの時、演出開始
 		if (white_out == true)
 		{
-			alpha_w += 0.02f;
+			alpha_w +=	PLUS__ALPHA_WHITE;
 		}
 		else
 		{
-			alpha_w -= 0.02f;
+			alpha_w -= PLUS__ALPHA_WHITE;
 			if (w_end == false)
 			{
-				scene_num -= 1;
-				alpha_b = 1.0f;
+				scene_num --;
+				alpha_b = INIT_ALPHA_BLACK;
 				w_end = true;
 			}
 		}
 
-		if (alpha_w >= 1.0f)
+		if (alpha_w >= MAX_ALPHA)
 			white_out = false;
 
 
-		if (alpha_w <= 0.0f)
+		if (alpha_w <= INIT_ALPHA_WHITE)
 		{
 			w_end = false;
-			alpha_w = 0.0f;
+			alpha_w = INIT_ALPHA_WHITE;
 			white = false;
 		}
 
 		Draw::Draw(8, &src_w, &dst_w, c_w, 0.0f);
 	}
 
-	if (scene_num < 2)
+	if (scene_num <= BG_LAST)
 	{
 		//描画カラー情報 R=RED　G=Green　B=Blue　A=alpha(透過情報)
 		float  c[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
-		RECT_F src;	//描画元切り取り位置
-		RECT_F dst;	//描画先表示位置
+		RECT_F src_clear;	//描画元切り取り位置
+		RECT_F dst_clear;	//描画先表示位置
 
-		//背景
+		//背景-----------------------
 		//切り取り位置の設定
-		src.m_top = 200.0f;
-		src.m_left = 150.0f;
-		src.m_right = 600.0f;
-		src.m_bottom = 450.0f;
+		src_clear.m_top = CLEAR_BG_SRC_TOP;
+		src_clear.m_left = CLEAR_BG_SRC_LEFT;
+		src_clear.m_right = src_clear.m_left + CLEAR_BG_X_SIZE;
+		src_clear.m_bottom = src_clear.m_top + CLEAR_BG_Y_SIZE;
 		//表示位置の設定
-		dst.m_top = 0.0f;
-		dst.m_left = 0.0f;
-		dst.m_right = dst.m_left + 800.0f;
-		dst.m_bottom = dst.m_top + 600.0f;
-		Draw::Draw(1, &src, &dst, c, 0.0f);
+		dst_clear.m_top = 0.0f;
+		dst_clear.m_left = 0.0f;
+		dst_clear.m_right = dst_clear.m_left + BG_X_SIZE;
+		dst_clear.m_bottom = dst_clear.m_top + BG_Y_SIZE;
+		Draw::Draw(scene_num, &src_clear, &dst_clear, c, 0.0f);
 
 		
-		//テキスト
+		//テキスト-------------------
 		//切り取り位置の設定
-		src.m_top = 0.0f;
-		src.m_left = 0.0f;
-		src.m_right = 700.0f;
-		src.m_bottom = 200.0f;
+		src_clear.m_top = 0.0f;
+		src_clear.m_left = 0.0f;
+		src_clear.m_right = src_clear.m_left + CLEAR_TEXT_X_SIZE;
+		src_clear.m_bottom = src_clear.m_top + CLEAR_TEXT_Y_SIZE;
 		//表示位置の設定
-		dst.m_top = 50.0f;
-		dst.m_left = 50.0f;
-		dst.m_right = dst.m_left + 700.0f;
-		dst.m_bottom = dst.m_top + 200.0f;
-		Draw::Draw(9, &src, &dst, c, 0.0f);
+		dst_clear.m_top = CLEAR_TEXT_DST_TOP;
+		dst_clear.m_left = CLEAR_TEXT_DST_LEFT;
+		dst_clear.m_right = dst_clear.m_left + CLEAR_TEXT_X_SIZE;
+		dst_clear.m_bottom = dst_clear.m_top + CLEAR_TEXT_Y_SIZE;
+		Draw::Draw(9, &src_clear, &dst_clear, c, 0.0f);
 
 
-		//主人公
+		//主人公---------------------
 		//切り取り位置の設定
-		src.m_top = 0.0f;
-		src.m_left = 0.0f + (h_ani_frame * 64.0f);
-		src.m_right = src.m_left + 64.0f;
-		src.m_bottom = src.m_top + 128.0f;
+		src_clear.m_top = 0.0f;
+		src_clear.m_left = 0.0f + (h_ani_frame * CLEAR_HERO_X_SIZE);
+		src_clear.m_right = src_clear.m_left + CLEAR_HERO_X_SIZE;
+		src_clear.m_bottom = src_clear.m_top + CLEAR_HERO_Y_SIZE;
 		//表示位置の設定
-		dst.m_top = 300.0f;
-		dst.m_left = 336.0f;
-		dst.m_right = dst.m_left + 64.0f;
-		dst.m_bottom = dst.m_top + 128.0f;
-		Draw::Draw(10, &src, &dst, c, 0.0f);
+		dst_clear.m_top = CLEAR_HERO_DST_TOP;
+		dst_clear.m_left = CLEAR_HERO_DST_LEFT;
+		dst_clear.m_right = dst_clear.m_left + CLEAR_HERO_X_SIZE;
+		dst_clear.m_bottom = dst_clear.m_top + CLEAR_HERO_Y_SIZE;
+		Draw::Draw(10, &src_clear, &dst_clear, c, 0.0f);
 	}
 
 }
--- LampGame/LampGame/ObjClear.h
+++ LampGame/LampGame/ObjClear.h
@@ -4,6 +4,46 @@
 //使用するネームスペース
 using namespace GameL;
 
+#define BG_NUM (7)
+#define BG_LAST (1)
+
+#define INIT_ALPHA_BLACK (1.0f)
+#define INIT_ALPHA_WHITE (0.0f)
+#define MAX_ALPHA (1.0f)
+
+#define INIT_H_ANI_TIME (0)
+#define INIT_H_ANI_FRAME (0)
+
+#define MAX_H_ANI_TIME (10)
+#define MAX_H_ANI_FRAME (4)
+
+#define PLUS__ALPHA_BLACK (0.005f)
+#define PLUS__ALPHA_WHITE (0.02f)
+
+#define SRC_BLACK_TOP (600.0f)
+#define SRC_BLACK_LEFT (0.0f)
+#define SRC_WHITE_TOP (0.0f)
+#define SRC_WHITE_LEFT (0.0f)
+
+#define BG_X_SIZE (800.0f)
+#define BG_Y_SIZE (600.0f)
+
+#define CLEAR_BG_SRC_TOP (200.0f)
+#define CLEAR_BG_SRC_LEFT (150.0f)
+#define CLEAR_BG_X_SIZE (450.0f)
+#define CLEAR_BG_Y_SIZE (250.0f)
+
+#define CLEAR_TEXT_DST_TOP (50.0f)
+#define CLEAR_TEXT_DST_LEFT (50.0f)
+#define CLEAR_TEXT_X_SIZE (700.0f)
+#define CLEAR_TEXT_Y_SIZE (200.0f)
+
+#define CLEAR_HERO_DST_TOP (300.0f)
+#define CLEAR_HERO_DST_LEFT (336.0f)
+#define CLEAR_HERO_X_SIZE (64.0f)
+#define CLEAR_HERO_Y_SIZE (128.0f)
+
+
 //オブジェクト：タイトル
 class CObjClear : public CObj
 {
